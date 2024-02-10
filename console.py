232db386a6ea9f1acccd4f86d20767d555c2e2ec
Celest21 <zc.chetty@gmail.com>
2024-02-10 13:25:45 -0800

    console fix

--- console.py
+++ console.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
 
 import cmd
+
+
 class HBNBCommand(cmd.Cmd):
-    """
-    HBNBCommand console class
-    """
-    prompt = "(hbnb) "
-    valid_classes = ["BaseModel", "User", "Amenity",
-                     "Place", "Review", "State", "City"]
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        '''<Quit> Command To Exit The Program'''
+        return True
+
+    def do_EOF(self, args):
+        '''Handles end of file'''
+        return True
 
     def emptyline(self):
-        """
-        Do nothing when an empty line is entered.
-        """
+        '''dont execute anything when the user
+           presses enter on an empty line
+        '''
         pass
-
-    def do_EOF(self, arg):
-        """
-        EOF (Ctrl+D) signal to exit the program.
-        """
-        return True
-
-    def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
-        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
