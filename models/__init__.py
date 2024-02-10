72b39e4389f68c3f34c760f4aa6ca172c42e2411
Celest21 <zc.chetty@gmail.com>
2024-02-10 01:04:04 -0800

    task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+from models.engine.file_storage import FileStorage
+
+"""Create a unique FileStorage instance for your applicatio"""
+storage = FileStorage()
+
+"""Call reload() method on this variable"""
+storage.reload()
