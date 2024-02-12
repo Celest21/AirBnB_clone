d4fc59ffbb7f993665e85ab12ac6ec53ea7f42dc
Celest21 <zc.chetty@gmail.com>
2024-02-12 10:59:42 -0800

    error fix

--- models/user.py
+++ models/user.py
@@ -15,13 +15,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """
-        Constructor for User class.
-        """
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', "")
-        self.password = kwargs.get('password', "")
-        self.first_name = kwargs.get('first_name', "")
-        self.last_name = kwargs.get('last_name', "")
