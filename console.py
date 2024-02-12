edba19cec5b81c35421318830a544b1452ae695f
Celest21 <zc.chetty@gmail.com>
2024-02-12 14:10:12 -0800

    task 11

--- console.py
+++ console.py
@@ -7,10 +7,10 @@ import cmd
 import shlex
 from models.user import User
 from models.state import State
-from model.city import City
-from model.amenity import Amenity
-from model.place import Place
-from model.review import Review
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
