f12ce0f305f37a26bf426476483dfa2e716538eb
Celest21 <zc.chetty@gmail.com>
2024-02-12 15:04:03 -0800

    error fix

--- models/user.py
+++ models/user.py
@@ -10,9 +10,7 @@ class User(BaseModel):
     """
     User class that inherits from BaseModel.
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
